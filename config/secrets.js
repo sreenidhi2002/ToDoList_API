3f9cef29c7998a8838b543c9c67f2dd5f99c2983
sreenidhivijay <sreenidhi5102@gmail.com>
2022-11-11 18:35:38 -0600

    CS409 MP3

--- config/secrets.js
+++ config/secrets.js
@@ -3,6 +3,6 @@
  */
 module.exports = {
     token : "secret-starter-mern",
-    mongo_connection : "YOUR URL TO MONGODB"
+    mongo_connection : "mongodb+srv://sreenidhi:HareKrishna@mp3.vkifbpy.mongodb.net/?retryWrites=true&w=majority"
     //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
 };
--- models/user.js
+++ models/user.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 
 // Define our user schema
 var UserSchema = new mongoose.Schema({
-    name: String
+    // Users cannot be created (or updated) without a name or email.
+    // All other fields that the user did not specify should be set to reasonable values.
+    name: {type: String, required: true},
+    // Multiple users with the same email cannot exist.
+    email: {type: String, index: {unique: true}, required: true},
+    pendingTasks: {type:[String], default:[]},
+    dateCreated: {type: Date, default: Date.now}
 });
 
 // Export the Mongoose model
